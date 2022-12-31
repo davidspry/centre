0f34b6bb7f38120958434f4e5ab2487edf5dc669
David Spry <dspry@gmx.com>
2022-12-31 11:30:03 +1100

    Initial Commit

---
+++ Centre/CentreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentreApp.swift
+//  Centre
+//
+//  Created by David Spry on 31/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CentreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
